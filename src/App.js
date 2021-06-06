8800e28210651e7b19d77236343a5381e7e8df61
sangha ju <sanghaju@sanghaui-MacBookPro.local>
2021-06-06 17:01:29 +0900

    useCallback, React.memo test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useMemo, useRef, useState } from "react";
-import CreateUsers from "./CreateUsers";
+import React, { useRef, useState, useMemo, useCallback } from "react";
+import CreateUser from "./CreateUsers";
 import UserList from "./UserList";
 
 function App() {
@@ -31,43 +31,45 @@ function App() {
 
   const { username, email } = inputs;
 
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value,
+      });
+    },
+    [inputs],
+  );
 
   const nextId = useRef(4);
 
-  const onCreate = (e) => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
       email,
     };
-
-    setUsers({ ...users, user });
+    setUsers((users) => users.concat(user));
     setInputs({
       username: "",
       email: "",
     });
-
+    console.log(nextId.current);
     nextId.current += 1;
-  };
+  }, [username, email, users]);
 
-  const onRemove = (id) => {
-    setUsers(users.filter((user) => user.id !== id));
-  };
+  const onRemove = useCallback((id) => {
+    setUsers((users) => users.filter((user) => user.id !== id));
+  }, []);
 
-  const onToggle = (id) => {
-    setUsers(
+  const onToggle = useCallback((id) => {
+    setUsers((users) =>
       users.map((user) =>
         user.id === id ? { ...user, active: !user.active } : user,
       ),
     );
-  };
+  }, []);
 
   const countActiveUsers = (users) => {
     console.log("활성 사용자 수 세는 중...");
@@ -77,16 +79,16 @@ function App() {
   const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
-    <div className="App">
-      <CreateUsers
+    <>
+      <CreateUser
         username={username}
         email={email}
         onChange={onChange}
         onCreate={onCreate}
       />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
-      <div>활성 사용자 수 : {count}</div>
-    </div>
+      <div>활성 사용자 수: {count}</div>
+    </>
   );
 }
 
--- src/UserList.js
+++ src/UserList.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function User({ user, onRemove, onToggle }) {
+const User = React.memo(function User({ user, onRemove, onToggle }) {
   const { username, email, id, active } = user;
+
+  useEffect(() => {
+    console.log("user 값이 설정 됨");
+    console.log(user);
+    return () => {
+      console.log("user 값이 바뀌기 전");
+      console.log("user 값이 설정 됨");
+    };
+  }, [user]);
 
   return (
     <div>
@@ -18,7 +27,7 @@ function User({ user, onRemove, onToggle }) {
       <button onClick={() => onRemove(id)}>삭제</button>
     </div>
   );
-}
+});
 
 function UserList({ users, onRemove, onToggle }) {
   return (
@@ -35,4 +44,4 @@ function UserList({ users, onRemove, onToggle }) {
   );
 }
 
-export default UserList;
+export default React.memo(UserList);
