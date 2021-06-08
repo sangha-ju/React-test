b4e30ec5c66b8b6ef0ab0e7a371d500bd0889701
sangha ju <sanghaju@sanghaui-MacBookPro.local>
2021-06-08 14:33:30 +0900

    reducer add immer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useReducer, useMemo, useCallback, createContext } from "react";
 import CreateUser from "./CreateUsers";
 import UserList from "./UserList";
+import produce from "immer";
 
 const initialState = {
   users: [
@@ -28,21 +29,32 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case "CREATE_USER":
-      return {
-        users: state.users.concat(action.user),
-      };
+      return produce(state, (draft) => {
+        draft.users.push(action.user);
+      });
+    // return {
+    //   users: state.users.concat(action.user),
+    // };
     case "TOGGLE_USER":
-      return {
-        ...state,
-        users: state.users.map((user) =>
-          user.id === action.id ? { ...user, active: !user.active } : user,
-        ),
-      };
+      return produce(state, (draft) => {
+        const user = draft.users.find((user) => user.id === action.id);
+        user.active = !user.active;
+      });
+    // return {
+    //   ...state,
+    //   users: state.users.map((user) =>
+    //     user.id === action.id ? { ...user, active: !user.active } : user,
+    //   ),
+    // };
     case "REMOVE_USER":
-      return {
-        ...state,
-        users: state.users.filter((user) => user.id !== action.id),
-      };
+      return produce(state, (draft) => {
+        const index = draft.users.findIndex((user) => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
+    // return {
+    //   ...state,
+    //   users: state.users.filter((user) => user.id !== action.id),
+    // };
     default:
       throw new Error("Unhandled action");
   }
